f2bdb460a11a0e474e026e7234190f9d128f4777
Ankith <ankithmanjesh@gmail.com>
2025-08-25 11:36:14 +0530

    Added DbContext from EF Core

---
+++ AGWalks/AGWalks.API/Data/AGWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AGWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AGWalks.API.Data
+{
+    public class AGWalksDbContext : DbContext
+    {
+        public AGWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties{ get; set; }
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Walk> Walks{ get; set; }
+    }
+}
