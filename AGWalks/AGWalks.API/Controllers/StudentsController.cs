43e5fa28d8cf969ebda0da85fffbd3eae7392273
Ankith <ankithmanjesh@gmail.com>
2025-08-25 10:45:49 +0530

    Adding Student Controller

---
+++ AGWalks/AGWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGWalks.API.Controllers
+{
+    //https://localhost:7097/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //https://localhost:7097/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Jack", "Ankith", "Anvi" };
+
+            return Ok(studentNames);
+        }
+    }
+}
--- AGWalks/AGWalks.API/obj/Debug/net8.0/AGWalks.API.AssemblyInfo.cs
+++ AGWalks/AGWalks.API/obj/Debug/net8.0/AGWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AGWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8b6b2c2f2e6c244a4333b7ce21d8ea5a97851c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AGWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AGWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
