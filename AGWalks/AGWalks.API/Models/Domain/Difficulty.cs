67727925b94a57c732dff5b17355670e01ae9f73
Ankith <ankithmanjesh@gmail.com>
2025-08-25 11:09:40 +0530

    Included Models

---
+++ AGWalks/AGWalks.API/Models/Domain/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace AGWalks.API.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
--- AGWalks/AGWalks.API/obj/Debug/net8.0/AGWalks.API.AssemblyInfo.cs
+++ AGWalks/AGWalks.API/obj/Debug/net8.0/AGWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AGWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8b6b2c2f2e6c244a4333b7ce21d8ea5a97851c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e5fa28d8cf969ebda0da85fffbd3eae7392273")]
 [assembly: System.Reflection.AssemblyProductAttribute("AGWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AGWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
