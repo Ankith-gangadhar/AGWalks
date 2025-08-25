4e11a6ba226b115472585a341ed211aaa967dae1
Ankith <ankithmanjesh@gmail.com>
2025-08-26 00:35:41 +0530

    Added Migrations and Created Image Controller

---
+++ AGWalks/AGWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,43 @@
+﻿using AGWalks.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST: api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageRequestUploadDto request)
+        {
+            ValidateFileUpload(request);
+
+            if(ModelState.IsValid)
+            {
+
+            }
+            return BadRequest(ModelState);
+
+        }
+
+        private void ValidateFileUpload(ImageRequestUploadDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".gif" };
+            
+            if(!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("File", "Unsupported file extension");
+            }
+
+            if(request.File.Length > 10 * 1024 * 1024) 
+            {
+                ModelState.AddModelError("File", "File size exceeds the 10MB limit");
+            }
+        }
+
+
+    }
+}
---
+++ AGWalks/AGWalks.API/Models/Domain/Image.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AGWalks.API.Models.Domain
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        [NotMapped]
+        public IFormFile File { get; set; }
+        public string Filename { get; set; }
+        public string? FileDescription { get; set; }
+        public string FileExtension{ get; set; }
+        public long FileSizeInBytes { get; set; }
+        public string Filepath { get; set; }
+    }
+}
