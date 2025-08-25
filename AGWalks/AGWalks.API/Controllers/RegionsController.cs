31100b871dfaa6a3c23769b3a1c406c1692916d8
Ankith <ankithmanjesh@gmail.com>
2025-08-25 12:47:37 +0530

    Region Controller Included

---
+++ AGWalks/AGWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,29 @@
+﻿using AGWalks.API.Data;
+using AGWalks.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGWalks.API.Controllers
+{
+    //https://localhost:7097/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly AGWalksDbContext dbContext;
+
+        public RegionsController(AGWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //GET ALL REGIONS
+        //GET: https://localhost:7097/api/regions
+        [HttpGet]
+        public IActionResult GetAll ()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+    }
+}
