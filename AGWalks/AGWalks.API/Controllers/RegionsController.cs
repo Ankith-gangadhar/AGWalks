7c3cc37358cc48c7474de566c4b691ef0935bbf0
Ankith <ankithmanjesh@gmail.com>
2025-08-25 13:31:32 +0530

    Added DTO to differentiate from Domains

--- AGWalks/AGWalks.API/Controllers/RegionsController.cs
+++ AGWalks/AGWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AGWalks.API.Data;
 using AGWalks.API.Models.Domain;
+using AGWalks.API.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,14 +17,58 @@ namespace AGWalks.API.Controllers
         {
             this.dbContext = dbContext;
         }
+
         //GET ALL REGIONS
         //GET: https://localhost:7097/api/regions
         [HttpGet]
         public IActionResult GetAll ()
         {
-            var regions = dbContext.Regions.ToList();
-            return Ok(regions);
+            //Get data from database - Domain Models
+            var regionsDomain = dbContext.Regions.ToList();
+
+            //Map domain models to DTO
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+            
+            //Return DTOs
+            return Ok(regionsDto);
         }
 
+        //GET SINGLE REGION (Get Region by ID)
+        //GET: https://localhost:7097/api/regions{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id); find is used specifically for primary keys
+            //Get region Data from database -- Domain Model
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id); //Can be used as universal
+
+            if (regionDomain == null)
+            {   
+                return NotFound();
+            }
+
+            //Map domain models to DTO
+            var regiondto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            //Return DTO to client
+            return Ok(regiondto);
+        }
     }
 }
