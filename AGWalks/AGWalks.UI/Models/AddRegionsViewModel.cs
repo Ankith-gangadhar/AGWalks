1aa8728257f62090464ebe32b7e349544fc2272a
Ankith <ankithmanjesh@gmail.com>
2025-08-26 13:48:06 +0530

    CRUD operatiosn implemented for UI for Region Controller

---
+++ AGWalks/AGWalks.UI/Models/AddRegionsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AGWalks.UI.Models
+{
+    public class AddRegionsViewModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
